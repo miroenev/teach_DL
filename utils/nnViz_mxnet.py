119b5f4c20436bec72c789c0783c96358a4c53d5
miroenev <miro@cs.washington.edu>
2017-11-27 09:46:15 -0800

    adding mxnet vis + demo

---
+++ utils/nnViz_mxnet.py
@@ -0,0 +1,166 @@
+# todo color with pos & negative
+# table with samples
+# color width 
+# + plot bias
+from matplotlib import pyplot
+from math import cos, sin, atan
+import numpy as np
+from customcolors import * 
+
+vertical_distance_between_layers = 5.5
+horizontal_distance_between_neurons = 2
+neuron_radius = 0.75
+number_of_neurons_in_widest_layer = 4 
+
+class Neuron():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y      
+
+    def draw(self):
+        global neuron_radius
+        circle = pyplot.Circle((self.x, self.y), radius=neuron_radius, fill=True)
+        circle.set_edgecolor((0,0,0))
+        circle.set_facecolor((1,1,1))
+        circle.set_linewidth(2)
+        circle.set_zorder(2)
+        pyplot.gca().add_patch(circle)
+
+
+
+class Layer():
+    def __init__(self, network, number_of_neurons, weights):
+        self.previous_layer = self.__get_previous_layer(network)
+        self.y = self.__calculate_layer_y_position()
+        self.neurons = self.__intialise_neurons(number_of_neurons)
+        self.weights = weights
+        self.maxWeight = 5
+        self.minWeight = .1
+
+
+
+    def __intialise_neurons(self, number_of_neurons):
+        global horizontal_distance_between_neurons
+        neurons = []
+        x = self.__calculate_left_margin_so_layer_is_centered(number_of_neurons)
+        for iteration in range(number_of_neurons):
+            neuron = Neuron(x, self.y)
+            neurons.append(neuron)
+            x += horizontal_distance_between_neurons
+        return neurons
+
+    def __calculate_left_margin_so_layer_is_centered(self, number_of_neurons):
+        global horizontal_distance_between_neurons
+        global number_of_neurons_in_widest_layer
+        return horizontal_distance_between_neurons * (number_of_neurons_in_widest_layer - number_of_neurons) / 2
+
+    def __calculate_layer_y_position(self):
+        global vertical_distance_between_layers
+        if self.previous_layer:
+            return self.previous_layer.y + vertical_distance_between_layers
+        else:
+            return 0
+
+    def __get_previous_layer(self, network):
+        if len(network.layers) > 0:
+            return network.layers[-1]
+        else:
+            return None
+
+    def __line_between_two_neurons(self, neuron1, neuron2, linewidth, sign):
+        global neuron_radius
+        angle = atan((neuron2.x - neuron1.x) / float(neuron2.y - neuron1.y))
+        x_adjustment = neuron_radius * sin(angle)
+        y_adjustment = neuron_radius * cos(angle)
+        line_x_data = (neuron1.x - x_adjustment, neuron2.x + x_adjustment)
+        line_y_data = (neuron1.y - y_adjustment, neuron2.y + y_adjustment)
+
+        if sign > 0:
+            #lineColor = (0,0,.5)
+            #lineColor = amazonSquidInk
+            lineColor = nikeGreen
+        else:
+            #lineColor = (.5,0,0)
+            #lineColor = amazonOrange
+            lineColor = nikeBlack
+
+        line = pyplot.Line2D(line_x_data, line_y_data, linewidth=linewidth, color=lineColor, alpha=.4)
+        line.set_zorder(1)
+        pyplot.gca().add_line(line)
+
+    def draw(self):
+        for this_layer_neuron_index in range(len(self.neurons)):
+            neuron = self.neurons[this_layer_neuron_index]
+
+            if self.previous_layer:
+                for previous_layer_neuron_index in range(len(self.previous_layer.neurons)):
+                    previous_layer_neuron = self.previous_layer.neurons[previous_layer_neuron_index]
+
+                    if self.previous_layer.weights is not None:
+                        rawWeight = self.previous_layer.weights[this_layer_neuron_index, previous_layer_neuron_index]
+                        if rawWeight > 0:
+                            sign = 1
+                        else:
+                            sign = -1
+
+                        processedWeight = abs(self.previous_layer.weights[this_layer_neuron_index, previous_layer_neuron_index]) # * 2 #+ .1
+                        
+                        weight = min ( (self.maxWeight , max( self.minWeight,  processedWeight)) )
+                        
+                        #print ( str(weight) + ', ' + str( rawWeight ))
+                        #weight = np.min( (self.maxWeight, self.previous_layer.weights[this_layer_neuron_index, previous_layer_neuron_index]) ) 
+                    else:
+                        weight = self.minWeight
+                    self.__line_between_two_neurons(neuron, previous_layer_neuron, weight, sign)
+            neuron.draw()
+
+
+
+class NeuralNetwork():
+    def __init__(self):
+        self.layers = []
+
+    def add_layer(self, number_of_neurons, weights=None):
+        layer = Layer(self, number_of_neurons, weights)
+        self.layers.append(layer)
+
+    def draw(self):        
+        for layer in self.layers:
+            layer.draw()
+        pyplot.axis('scaled')
+        pyplot.axis('off')
+        pyplot.show()
+
+
+def visualize_model( module ):
+    modelParams = module.get_params()[0]
+    nLayers = 0
+    finalLayerSize = 0
+    modelShape = {}
+    modelWeights = {}
+    pyplot.figure(figsize=(10,10))
+    
+    for k,v in iter(sorted(modelParams.items())):
+        if 'weight' in k.lower():
+            modelShape[nLayers] = v.shape[1]
+            modelWeights[nLayers] = v.asnumpy()
+            finalLayerSize = v.shape[0]
+            nLayers += 1
+            #print( v.shape)
+            #print( v.asnumpy() )
+
+    network = NeuralNetwork()
+
+    # first layer
+    network.add_layer( modelShape[0], modelWeights[0])
+
+    for iLayer in range( 1, nLayers ):    
+        network.add_layer( modelShape[iLayer], modelWeights[iLayer])
+
+    # last layer
+    network.add_layer( finalLayerSize )
+
+    network.draw()
+
+if __name__ == "__main__":
+    print('main')
